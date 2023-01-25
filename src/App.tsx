4578e7a152efe90d0f87eb7f282fe24b7bbe66d5
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-01-25 19:32:44 +0000

    exercises 1 to 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/navbar';
 import Header from './components/header';
 import Footer from './components/footer';
+import Cat from './data/cat';
+import CatCard from './components/cat_card';
+import catData from './data/cat-data';
+
 
 function App(): JSX.Element {
 
 	// JavaScript/TypeScript code can be inserted here!
+	const [cats, setCats] = useState<Array<Cat>>(catData);
+
+	console.log("Our pretties 😻: ", cats);
+	const catCount = cats.length;
+	console.log("Number of 😻: ", catCount);
 
 	return (
 		<>
 			<Navbar />
-			<Header />
+			<Header catCount={catCount} />
 
 			<main>
-				<div className='cards__wrapper'>{/* JSX code can go here */}</div>
+				<div className='cards__wrapper'>
+					{cats.map((cat, index) => <CatCard
+						key={cat.id}
+						name={cat.name}
+						species={cat.species}
+						favFoods={cat.favFoods}
+						birthYear={cat.birthYear}
+						catIndex={index}
+					/>)}
+				</div>
 			</main>
 
 			<Footer />
