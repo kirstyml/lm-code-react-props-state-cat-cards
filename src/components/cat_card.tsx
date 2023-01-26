45dfb866fd2df053b476280bfc66c4013afa6671
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-01-26 16:53:08 +0000

    listed out favourite foods

--- src/components/cat_card.tsx
+++ src/components/cat_card.tsx
@@ -13,130 +13,135 @@ import cat11 from '../assets/images/cat11.jpg';
 import cat12 from '../assets/images/cat12.jpg';
 
 const images = [
-	{
-		image: cat1,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY-SA 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by-sa/2.0/',
-		attributionName: 'amblin',
-		attributionUrl: 'https://www.flickr.com/people/amblin/'
-	},
-	{
-		image: cat2,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY-SA 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by-sa/2.0/',
-		attributionName: 'ivva',
-		attributionUrl: 'https://www.flickr.com/people/ivva/'
-	},
-	{
-		image: cat3,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY-ND 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by-nd/2.0/',
-		attributionName: 'Rachele Pettarelli Ph',
-		attributionUrl: 'https://www.flickr.com/people/rachephotos/'
-	},
-	{
-		image: cat4,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by/2.0/',
-		attributionName: 'renarl',
-		attributionUrl: 'https://www.flickr.com/people/renarl/'
-	},
-	{
-		image: cat5,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY-SA 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by-sa/2.0/',
-		attributionName: 'x-oph',
-		attributionUrl: 'https://www.flickr.com/people/x-oph/'
-	},
-	{
-		image: cat6,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY-ND 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by-nd/2.0/',
-		attributionName: 'arrathoonlaa@att.net',
-		attributionUrl: 'https://www.flickr.com/people/21851382@N04/'
-	},
-	{
-		image: cat7,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by/2.0/',
-		attributionName: 'pavlovskyy',
-		attributionUrl: 'https://www.flickr.com/people/pavlovskyy/'
-	},
-	{
-		image: cat8,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by/2.0/',
-		attributionName: 'AleGranholm',
-		attributionUrl: 'https://www.flickr.com/people/darthale/'
-	},
-	{
-		image: cat9,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by/2.0/',
-		attributionName: 'Sardonic G',
-		attributionUrl: 'https://www.flickr.com/people/24039825@N06/'
-	},
-	{
-		image: cat10,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by/2.0/',
-		attributionName: 'www.metaphoricalplatypus.com',
-		attributionUrl: 'https://www.flickr.com/people/29638108@N06/'
-	},
-	{
-		image: cat11,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY-SA 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by-sa/2.0/',
-		attributionName: 'abraham.williams',
-		attributionUrl: 'https://www.flickr.com/people/4braham/'
-	},
-	{
-		image: cat12,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY-SA 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by-sa/2.0/',
-		attributionName: 'x-oph',
-		attributionUrl: 'https://www.flickr.com/people/x-oph/'
-	}
+    {
+        image: cat1,
+        altText: 'Describe this cat!',
+        licenceType: 'CC BY-SA 2.0',
+        licenceUrl: 'https://creativecommons.org/licenses/by-sa/2.0/',
+        attributionName: 'amblin',
+        attributionUrl: 'https://www.flickr.com/people/amblin/'
+    },
+    {
+        image: cat2,
+        altText: 'Describe this cat!',
+        licenceType: 'CC BY-SA 2.0',
+        licenceUrl: 'https://creativecommons.org/licenses/by-sa/2.0/',
+        attributionName: 'ivva',
+        attributionUrl: 'https://www.flickr.com/people/ivva/'
+    },
+    {
+        image: cat3,
+        altText: 'Describe this cat!',
+        licenceType: 'CC BY-ND 2.0',
+        licenceUrl: 'https://creativecommons.org/licenses/by-nd/2.0/',
+        attributionName: 'Rachele Pettarelli Ph',
+        attributionUrl: 'https://www.flickr.com/people/rachephotos/'
+    },
+    {
+        image: cat4,
+        altText: 'Describe this cat!',
+        licenceType: 'CC BY 2.0',
+        licenceUrl: 'https://creativecommons.org/licenses/by/2.0/',
+        attributionName: 'renarl',
+        attributionUrl: 'https://www.flickr.com/people/renarl/'
+    },
+    {
+        image: cat5,
+        altText: 'Describe this cat!',
+        licenceType: 'CC BY-SA 2.0',
+        licenceUrl: 'https://creativecommons.org/licenses/by-sa/2.0/',
+        attributionName: 'x-oph',
+        attributionUrl: 'https://www.flickr.com/people/x-oph/'
+    },
+    {
+        image: cat6,
+        altText: 'Describe this cat!',
+        licenceType: 'CC BY-ND 2.0',
+        licenceUrl: 'https://creativecommons.org/licenses/by-nd/2.0/',
+        attributionName: 'arrathoonlaa@att.net',
+        attributionUrl: 'https://www.flickr.com/people/21851382@N04/'
+    },
+    {
+        image: cat7,
+        altText: 'Describe this cat!',
+        licenceType: 'CC BY 2.0',
+        licenceUrl: 'https://creativecommons.org/licenses/by/2.0/',
+        attributionName: 'pavlovskyy',
+        attributionUrl: 'https://www.flickr.com/people/pavlovskyy/'
+    },
+    {
+        image: cat8,
+        altText: 'Describe this cat!',
+        licenceType: 'CC BY 2.0',
+        licenceUrl: 'https://creativecommons.org/licenses/by/2.0/',
+        attributionName: 'AleGranholm',
+        attributionUrl: 'https://www.flickr.com/people/darthale/'
+    },
+    {
+        image: cat9,
+        altText: 'Describe this cat!',
+        licenceType: 'CC BY 2.0',
+        licenceUrl: 'https://creativecommons.org/licenses/by/2.0/',
+        attributionName: 'Sardonic G',
+        attributionUrl: 'https://www.flickr.com/people/24039825@N06/'
+    },
+    {
+        image: cat10,
+        altText: 'Describe this cat!',
+        licenceType: 'CC BY 2.0',
+        licenceUrl: 'https://creativecommons.org/licenses/by/2.0/',
+        attributionName: 'www.metaphoricalplatypus.com',
+        attributionUrl: 'https://www.flickr.com/people/29638108@N06/'
+    },
+    {
+        image: cat11,
+        altText: 'Describe this cat!',
+        licenceType: 'CC BY-SA 2.0',
+        licenceUrl: 'https://creativecommons.org/licenses/by-sa/2.0/',
+        attributionName: 'abraham.williams',
+        attributionUrl: 'https://www.flickr.com/people/4braham/'
+    },
+    {
+        image: cat12,
+        altText: 'Describe this cat!',
+        licenceType: 'CC BY-SA 2.0',
+        licenceUrl: 'https://creativecommons.org/licenses/by-sa/2.0/',
+        attributionName: 'x-oph',
+        attributionUrl: 'https://www.flickr.com/people/x-oph/'
+    }
 ];
 
-interface CatCardProps{
+interface CatCardProps {
     name: string;
     species: string;
     favFoods: Array<string>;
     birthYear: number;
     catIndex: number;
- }
+}
 
-const CatCard : React.FC<CatCardProps> = ({ name, species, favFoods, birthYear, catIndex }) => {
-        console.log(name);
-        return (
-            <div className="card">
-                <h3 className="card__text card__header">{name}</h3>
-                <p className="card__text">Species: {species}</p>
-                <p className="card__text">Favourite Food(s): {favFoods}</p>
-                <p className="card__text">Birth Year: {birthYear}</p>
-               {catIndex < images.length && <CatImage 
-                    image={images[catIndex].image}
-                    altText={images[catIndex].altText}
-                    licenceType={images[catIndex].licenceType}
-                    licenceUrl={images[catIndex].licenceUrl}
-                    attributionName={images[catIndex].attributionName}
-                    attributionUrl={images[catIndex].attributionUrl}
-                />}
-            </div>
-        );
+const CatCard: React.FC<CatCardProps> = ({ name, species, favFoods, birthYear, catIndex }) => {
+    console.log(name);
+    return (
+        <div className="card">
+            <h3 className="card__text card__header">{name}</h3>
+            <p className="card__text">Species: {species}</p>
+            <p className="card__text">Favourite Food(s): 
+                {favFoods.reduce((acc, curr, currIndex) => {
+                    currIndex === favFoods.length - 1 ? acc += curr : acc += `${curr}, `
+                    return acc;
+                }, "")}
+            </p>
+            <p className="card__text">Birth Year: {birthYear}</p>
+            {catIndex < images.length && <CatImage
+                image={images[catIndex].image}
+                altText={images[catIndex].altText}
+                licenceType={images[catIndex].licenceType}
+                licenceUrl={images[catIndex].licenceUrl}
+                attributionName={images[catIndex].attributionName}
+                attributionUrl={images[catIndex].attributionUrl}
+            />}
+        </div>
+    );
 }
 
 
